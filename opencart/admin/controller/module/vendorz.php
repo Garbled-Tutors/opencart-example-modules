42f92195a25ffedcd57be9de73e91658aa525c40
John Smith <private@unknown.com>
2013-03-13 14:41:09 -0700

    Gutted vendorz to a much smaller controller

--- opencart/admin/controller/module/vendorz.php
+++ opencart/admin/controller/module/vendorz.php
@@ -1,103 +1,50 @@
 <?php
 class ControllerModuleVendorz extends Controller {
-	private $error = array(); 
-	
-	public function index() {   
-		$this->load->language('module/vendorz');
+	private $error = array();
 
-		$this->document->setTitle($this->language->get('heading_title'));
-		
-		$this->load->model('setting/setting');
-				
-		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
-			$this->model_setting_setting->editSetting('vendorz', $this->request->post);		
-					
-			$this->session->data['success'] = $this->language->get('text_success');
-						
-			$this->redirect($this->url->link('extension/module', 'token=' . $this->session->data['token'], 'SSL'));
-		}
-				
-		$this->data['heading_title'] = $this->language->get('heading_title');
+	protected function get_bread_crumbs($human_name, $machine_name, $token)
+	{
+		$breadcrumbs = array();
+		$breadcrumbs[] = array(	'text' => 'Home', 			'href' => $this->url->link('common/home', 'token=' . $token, 'SSL'), 							'separator' => false	);
+		$breadcrumbs[] = array(	'text' => 'Modules', 		'href' => $this->url->link('extension/module', 'token=' . $token, 'SSL'),					'separator' => ' :: '	);
+		$breadcrumbs[] = array(	'text' => $human_name,	'href' => $this->url->link('module/' . $machine_name, 'token=' . $token, 'SSL'),	'separator' => ' :: '	);
+		return $breadcrumbs;
+	}
 
-		$this->data['text_enabled'] = $this->language->get('text_enabled');
-		$this->data['text_disabled'] = $this->language->get('text_disabled');
-		$this->data['text_content_top'] = $this->language->get('text_content_top');
-		$this->data['text_content_bottom'] = $this->language->get('text_content_bottom');		
-		$this->data['text_column_left'] = $this->language->get('text_column_left');
-		$this->data['text_column_right'] = $this->language->get('text_column_right');
-		
-		$this->data['entry_layout'] = $this->language->get('entry_layout');
-		$this->data['entry_position'] = $this->language->get('entry_position');
-		$this->data['entry_status'] = $this->language->get('entry_status');
-		$this->data['entry_sort_order'] = $this->language->get('entry_sort_order');
-		
-		$this->data['button_save'] = $this->language->get('button_save');
-		$this->data['button_cancel'] = $this->language->get('button_cancel');
-		$this->data['button_add_module'] = $this->language->get('button_add_module');
-		$this->data['button_remove'] = $this->language->get('button_remove');
+	public function index() {
+		$this->document->setTitle('Vendors');
 
- 		if (isset($this->error['warning'])) {
+		if (isset($this->error['warning'])) {
 			$this->data['error_warning'] = $this->error['warning'];
 		} else {
 			$this->data['error_warning'] = '';
 		}
 
-  		$this->data['breadcrumbs'] = array();
+		$this->data['breadcrumbs'] = $this->get_bread_crumbs('Vendors', 'vendorz', $this->session->data['token']);
 
-   		$this->data['breadcrumbs'][] = array(
-       		'text'      => $this->language->get('text_home'),
-			'href'      => $this->url->link('common/home', 'token=' . $this->session->data['token'], 'SSL'),
-      		'separator' => false
-   		);
-
-   		$this->data['breadcrumbs'][] = array(
-       		'text'      => $this->language->get('text_module'),
-			'href'      => $this->url->link('extension/module', 'token=' . $this->session->data['token'], 'SSL'),
-      		'separator' => ' :: '
-   		);
-		
-   		$this->data['breadcrumbs'][] = array(
-       		'text'      => $this->language->get('heading_title'),
-			'href'      => $this->url->link('module/vendorz', 'token=' . $this->session->data['token'], 'SSL'),
-      		'separator' => ' :: '
-   		);
-		
-		$this->data['action'] = $this->url->link('module/vendorz', 'token=' . $this->session->data['token'], 'SSL');
-		
-		$this->data['cancel'] = $this->url->link('extension/module', 'token=' . $this->session->data['token'], 'SSL');
-				
-		$this->data['modules'] = array();
-		
-		if (isset($this->request->post['vendorz_module'])) {
-			$this->data['modules'] = $this->request->post['vendorz_module'];
-		} elseif ($this->config->get('vendorz_module')) { 
-			$this->data['modules'] = $this->config->get('vendorz_module');
-		}
-						
 		$this->load->model('design/layout');
-		
+
 		$this->data['layouts'] = $this->model_design_layout->getLayouts();
-		
-		
+
 		$this->template = 'module/vendorz.tpl';
 		$this->children = array(
 			'common/header',
 			'common/footer'
 		);
-				
+
 		$this->response->setOutput($this->render());
 	}
-	
+
 	private function validate() {
 		if (!$this->user->hasPermission('modify', 'module/vendorz')) {
 			$this->error['warning'] = $this->language->get('error_permission');
 		}
-		
+
 		if (!$this->error) {
 			return true;
 		} else {
 			return false;
-		}	
+		}
 	}
 }
 ?>
