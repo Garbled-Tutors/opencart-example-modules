d71517177ceac80e268eb6782d8c705a0bb18dd9
John Smith <private@unknown.com>
2013-03-18 13:57:07 -0700

    Copied blank module into new module called multitable

---
+++ opencart/admin/language/english/module/multitable.php
@@ -0,0 +1,6 @@
+<?php
+$_['heading_title']    = 'Multitable Module';
+$_['text_module']      = 'Modules';
+$_['text_success']     = 'Success: You have modified module Multitable module!';
+$_['error_permission'] = 'Warning: You do not have permission to modify module Multitable module!';
+?>
